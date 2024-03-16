f225c2e2c919ff509c4dd240164957d28040c316
ZSandor-dev <157478742+ZSandor-dev@users.noreply.github.com>
2024-03-17 00:24:49 +0100

    Add files for review

---
+++ Source_code/Server.py
@@ -0,0 +1,137 @@
+"""
+Import pickle, socket and cryptography.fernet.
+"""
+import pickle
+import socket
+from cryptography.fernet import Fernet
+
+
+class Server:
+    """
+    This class represents a server that can receive data from the client.
+    """
+    def __init__(self, filename, dictionary_name):
+        """
+        Reserve a port, create a socket object, get local machine name,
+        bind to the port, and wait for the connection.
+        """
+        port = 60000
+        self.conn = False                     
+        self.s = socket.socket()
+        host = socket.gethostname()
+        self.s.bind((host, port))
+        self.s.listen(5)
+        self.filename = filename
+        self.dictionary_name = dictionary_name
+
+        print ('Server listening....')
+
+    def decDictionary(self, enc_dictionary, fernet):
+        """
+        Decode the encrypted dictionary that the server receives.
+        """
+        decDictionary = {}
+        for key, value in enc_dictionary.items():
+            dec_key = fernet.decrypt(key).decode()
+            dec_value = fernet.decrypt(value).decode()
+            decDictionary[dec_key] = dec_value
+
+        return decDictionary
+
+    def printDictionary(self, dec_dictionary):
+        """
+        Print the received dictionary.
+        """
+        print("Received dictionary content: ")
+        for key, value in dec_dictionary.items():
+            print("key: " , key, " " , "value: ", value)
+
+    def writeDictionaryToFile(self, dec_dictionary):
+        """
+        Write the dictionary into a file.
+        """
+        write_file = open(self.dictionary_name, 'w', encoding="utf-8")
+        count = 0
+
+        for key, value in dec_dictionary.items():
+            count +=1
+            if count < len(dec_dictionary):
+                write_file.write(f"{key}: {value}, ")
+            else:
+                write_file.write(f"{key}: {value}")
+        write_file.close()
+
+    def writeMsgToFile(self, dec_message):
+        """
+        Write the received file content to a file.
+        """
+        write_file =  open(self.filename, 'w', encoding="utf-8")
+        write_file.write(dec_message)
+        write_file.close()
+
+    def receiveData(self):
+        """
+        Establish connection and receive data from the client. 
+        """
+        listening = True
+        while listening:
+            self.conn, addr = self.s.accept()
+            print('Got connection from', addr)
+            data = self.conn.recv(4096)
+           
+            if not data:
+                listening = False
+            else:
+                # Deserialise data
+                data = pickle.loads(data)
+                key = data['key']
+                message = data['message']
+                write_file = data['write_file']
+                print_message = data['print_message']
+                encryption = data['encryption']
+                dictionary_format = data['dictionary_format']
+                # Initialise fernet object with encryption 
+                fernet = Fernet(key)
+
+                if dictionary_format == True:  # If data is in dictionary format
+                    received_dictionary = message
+
+                    if encryption == True:
+                        print("Coded Dictionary:" , received_dictionary)
+                        received_dictionary = self.decDictionary(received_dictionary, fernet)   # Decoding dictionary
+
+                    if print_message == True:
+                        self.printDictionary(received_dictionary)  # Print dictionary
+
+                    if write_file == True :
+                        self.writeDictionaryToFile(received_dictionary)   # Write dictionary content in a file
+
+                else:  # If the received data is not in dictionary format
+                    if encryption == True:  # Decrypt file content
+                        print("Coded message: ", message)
+                        message = fernet.decrypt(message).decode()
+                        print("Decoded message: ", message)
+ 
+                    if print_message == True:    # Print file content
+                        print("Message: ",  message)
+
+                    if write_file == True:     # Write file content into a file
+                        self.writeMsgToFile(message)
+
+                print('Successfully received') 
+
+
+    def closeConnection(self):
+        """
+        Close connection.
+        """
+        print ("Connection closed.")
+        self.conn.close()
+
+
+
+if __name__ == "__main__":
+    # Initialise the server  and defiine the files where we write the received data
+    server = Server('received_file.txt', 'received_dict.txt')  
+    server.receiveData()   # Start listening for client connection and receiving data                                                 
+    server.closeConnection()   # Close connection
---
+++ Tests/TestDictionary.py
@@ -0,0 +1,64 @@
+"""
+Import unittest, os, time and Client.
+"""
+import unittest
+import os
+import time
+from Client import Client
+
+class TestDataTransfer(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.received_dict_name = "received_dict.txt"
+        self.sending_dictionary = ''
+      
+
+    def test_DictionarySending(self):
+        """
+        Test if the server writes the sent data in a file
+        """ 
+        encryption = False
+        write_file = True
+        print_message = True
+        dictionary_format = True
+        self.sending_dictionary = self.client.createDictionary(encryption)
+        self.client.sendData(self.sending_dictionary, write_file, print_message, dictionary_format, encryption)
+
+        time.sleep(2.0)
+        self.assertTrue(os.path.exists(self.received_dict_name))
+    
+    def test_DictFileContent(self):
+        """
+        Test if the data sent by the client is identical with the data written in the file by the server
+        """
+        received_dict_content = ''
+
+        time.sleep(2.0)
+        try:
+            with open(self.received_dict_name, 'r', encoding="utf-8") as file:
+                received_dict_content = file.read()
+            file.close()
+
+        except FileNotFoundError:
+            print(f"Error found in the comparison of the file contents.")
+
+
+        if self.sending_dictionary != '' and  received_dict_content != '':
+            self.assertEqual(self.sending_dictionary, received_dict_content)
+        
+
+    
+if __name__ == '__main__':
+    """
+    Create a test suite 
+    """
+    suite = unittest.TestSuite()    
+    suite.addTest(TestDataTransfer('test_DictionarySending'))  # Add test case for dictionary sending
+    suite.addTest(TestDataTransfer('test_DictFileContent'))    # Add test case for dictionary file content
+    result = unittest.TestResult()                             # Create a test result object
+    suite.run(result)                                          # Run the test suite
+
+    # Print the test results
+    print("Number of tests run:", result.testsRun)
+    print("Number of failures:", len(result.failures))
+    print("Number of errors:", len(result.errors))
